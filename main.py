7b7aef63dbc42bf04d83dcb8d0b66d2ffe0611ff
Angela Nikolova <41633886+codekoya@users.noreply.github.com>
2021-11-17 14:04:44 +0100

    Changed comments in main.py

--- main.py
+++ main.py
@@ -4,31 +4,31 @@ import pytesseract
 pytesseract.pytesseract.tesseract_cmd='C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 img = cv2.imread('test.png')
 img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-# print(pytesseract.image_to_string(img))
+print(pytesseract.image_to_string(img))
 
-#detektiranje karakteri
-# hImg,wImg,_=img.shape
-# boxes = pytesseract.image_to_boxes(img)
-# for b in boxes.splitlines():
-#     b=b.split(' ')
-#     print(b)
-#     x,y,w,h=int(b[1]),int(b[2]),int(b[3]),int(b[4])
-#     cv2.rectangle(img,(x,hImg-y),(w,hImg-h),(0,0,255),1)
-#     cv2.putText(img,b[0],(x,hImg-y+30),cv2.FONT_HERSHEY_COMPLEX,1,(50,50,255),2)
+#detecting characters
+hImg,wImg,_=img.shape
+boxes = pytesseract.image_to_boxes(img)
+for b in boxes.splitlines():
+     b=b.split(' ')
+     print(b)
+     x,y,w,h=int(b[1]),int(b[2]),int(b[3]),int(b[4])
+     cv2.rectangle(img,(x,hImg-y),(w,hImg-h),(0,0,255),1)
+     cv2.putText(img,b[0],(x,hImg-y+30),cv2.FONT_HERSHEY_COMPLEX,1,(50,50,255),2)
 
-#detektiranje zborovi
-# hImg,wImg,_=img.shape
-# boxes = pytesseract.image_to_data(img)
-# for x,b in enumerate(boxes.splitlines()):
-#     if x!=0:
-#         b=b.split()
-#         print(b)
-#         if len(b)==12:
-#             x,y,w,h=int(b[6]),int(b[7]),int(b[8]),int(b[9])
-#             cv2.rectangle(img,(x,y),(w+x,h+y),(0,0,255),1)
-#             cv2.putText(img,b[11],(x,y),cv2.FONT_HERSHEY_COMPLEX,1,(50,50,255),2)
+#detecting words
+ hImg,wImg,_=img.shape
+ boxes = pytesseract.image_to_data(img)
+ for x,b in enumerate(boxes.splitlines()):
+     if x!=0:
+         b=b.split()
+         print(b)
+         if len(b)==12:
+             x,y,w,h=int(b[6]),int(b[7]),int(b[8]),int(b[9])
+             cv2.rectangle(img,(x,y),(w+x,h+y),(0,0,255),1)
+             cv2.putText(img,b[11],(x,y),cv2.FONT_HERSHEY_COMPLEX,1,(50,50,255),2)
 
-#detektiranje samo broevi
+#detecting only numbers
 hImg,wImg,_ = img.shape
 cong = r'--oem 3 --psm 6 outputbase digits'
 boxes = pytesseract.image_to_data(img,config=cong)
